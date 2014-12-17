d7a331eb864ae313f13dd14b76fdeaab8fe543b5
Rob Rothe <robrothe@gmail.com>
2014-12-17 09:47:04 -0600

    Removed files

--- apps/testapi/app.js
+++
@@ -1,8 +0,0 @@
-angular
-.module('testApp',[])
-.controller('TestCtrl',function($scope,$http) {
-	$http.jsonp('http://itunes.apple.com/lookup?id=994656&entity=album',{params:{'callback':'JSON_CALLBACK'}})
-	.then(function(response) {
-		$scope.data = response.data;
-	});
-})
