0699781e48378328b22a0bbeb7b3e183ec13900f
Rob Rothe <robrothe@gmail.com>
2015-06-26 13:33:31 -0500

    Not needed

--- examples/items/api/db_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// db connetion for api
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASS","brickwall233");
-define("DB_NAME","angularapps");
--- examples/items/app/js/directives.js
+++
@@ -1,8 +0,0 @@
-app.directive('spinner',function() {
-	var directive = {
-		templateUrl: '/items/app/partials/spinner.html',
-		restrict:'E',
-		scope:{ message:'@'}
-	}
-	return directive;
-})
