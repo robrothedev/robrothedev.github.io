164da4c9cf8b97cee8579c4fd2124954d7466e60
Rob Rothe <robrothe@gmail.com>
2016-01-23 08:50:48 -0600

    Updating sticky menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,25 +16,17 @@ $(document).ready(function() {
 	// bootstrap tooltips
 	$('[data-toggle=tooltip]').tooltip();
 
-	// sticky top menu
-	$('.top').hide();
-
+	// sticky navbar
 	$(document).scroll(function() {
 
-		var sticky = false;
-
 		if ($(document).scrollTop() > 100) {
-
-			/* show sticky menu */
-			$('#navbar').hide();
+			/* show sticky navbar */
+			$('#navbar').fadeOut('fast');
 			$('#navbar-sticky').fadeIn('fast');
 		}
-
-
-		if ($(document).scrollTop() < 20) {
-			$('.top').fadeOut();
-
-			/* sticky menu */
+ 
+		if ($(document).scrollTop() < 100) {
+			/* hide sticky navbar */
 			$('#navbar-sticky').fadeOut('fast');
 			$('#navbar').fadeIn('fast');
 		}
